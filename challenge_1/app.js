7c92474257a941ebb954d3f6a6937629ff8ff144
Wyatt <mrwnt10@gmail.com>
2018-12-17 11:37:03 -0500

    Add board table and initial click handlers for cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,7 @@
-
-
+// add click handlers to board cells
+var cells = document.querySelectorAll("td");
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", function() {
+    console.log("clicked");
+  });
+}

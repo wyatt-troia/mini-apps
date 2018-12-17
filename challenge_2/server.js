124f4cd49dbbc6da778ebe4c209233345395feb7
Wyatt <mrwnt10@gmail.com>
2018-12-17 17:35:22 -0500

    Add client form and Express server with post route

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const morgan = require("morgan");
 
+app.use(morgan("dev"));
 
+app.use(express.static(path.join(__dirname, "/client")));
+
+app.post("/JSON-to-CSV", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

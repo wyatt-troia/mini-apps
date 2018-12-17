50daf59dc0728f403db2e28418338b66cfa2f982
Wyatt <mrwnt10@gmail.com>
2018-12-17 15:17:29 -0500

    Add comments

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -29,17 +29,23 @@ var state = {
   }
 };
 
-// add click handlers to board cells
+// add click handlers to grid
 let html_board = document.getElementById("board");
 html_board.addEventListener("click", e => {
   if (e.target !== e.currentTarget) {
+    // get col and row index of clicked cell
     var rows = Array.from(document.getElementsByTagName("tr"));
     var rowIndex = rows.indexOf(event.target.parentNode);
     var cols = Array.from(event.target.parentNode.children);
     var colIndex = cols.indexOf(event.target);
-    console.log(`clicked row ${rowIndex}, col ${colIndex}`);
+
+    // update text in cell
     event.target.innerHTML = state.upNext;
+
+    // update state model of board
     state.board[rowIndex][colIndex] = state.upNext;
+
+    // check for and handle win or tie event
     if (hasWon("X")) {
       displayWinNotification("X");
       state.winner = "X";
@@ -59,7 +65,9 @@ html_board.addEventListener("click", e => {
       displayTieNotification();
       return;
     }
-    state.advanceTurn.call(state);
+
+    // update state in preparation for next turn
+    state.advanceTurn();
   }
   e.stopPropagation();
 });
@@ -110,6 +118,8 @@ function hasWon(player) {
     return true;
   return false;
 }
+
+let view = {};
 
 // display win notification
 function displayWinNotification(winner) {

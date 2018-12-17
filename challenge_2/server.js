a59639df67c848a2a17d6720e8c904359052ccb5
Wyatt <mrwnt10@gmail.com>
2018-12-17 17:56:49 -0500

    Correct HTML form and add correct POST body parsing

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(morgan("dev"));
 
 app.use(express.static(path.join(__dirname, "/client")));
 
-app.post("/JSON-to-CSV", (req, res) => {
+app.post("/JSON-to-CSV", urlencodedParser, (req, res) => {
+  console.log(JSON.parse(req.body.JSON));
   res.send("hello world");
 });
 

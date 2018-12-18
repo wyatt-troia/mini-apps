25eb03f772830de457a0e149b40068c4915eb8df
Wyatt <mrwnt10@gmail.com>
2018-12-18 16:39:01 -0500

    Working hello world

---
+++ challenge_3/client/src/app.jsx
@@ -0,0 +1,8 @@
+// import Checkout from './components/checkout';
+
+// ReactDOM.render(<Checkout />, document.getElementById("root"));
+
+ReactDOM.render(
+  <div>Hello, World! from React</div>,
+  document.getElementById("root")
+);

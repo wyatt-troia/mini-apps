f812c2a5919325bc1d1f389061276c82a30466a1
Wyatt <mrwnt10@gmail.com>
2018-12-18 21:31:51 -0500

    Add Knex

--- challenge_3/database/models.js
+++ challenge_3/database/models.js
@@ -1,6 +1,21 @@
-const db = require("./index.js");
+// const db = require("./index.js");
 
-db.query("SELECT * FROM purchases", function(error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ", results);
+// db.query("SELECT * FROM purchases", function(error, results, fields) {
+//   if (error) throw error;
+//   console.log("The solution is: ", results);
+// });
+
+var knex = require("knex")({
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "checkout"
+  }
 });
+
+knex
+  .select()
+  .table("purchases")
+  .then(result => console.log(result));

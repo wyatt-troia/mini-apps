ea72847778911d6d70b5ade7b0b4e17fdee2ecb8
Wyatt <mrwnt10@gmail.com>
2018-12-19 23:33:16 -0500

    Remove obsolete database index.js

--- challenge_3/database/models.js
+++ challenge_3/database/models.js
@@ -1,10 +1,3 @@
-// const db = require("./index.js");
-
-// db.query("SELECT * FROM purchases", function(error, results, fields) {
-//   if (error) throw error;
-//   console.log("The solution is: ", results);
-// });
-
 var knex = require("knex")({
   client: "mysql",
   connection: {

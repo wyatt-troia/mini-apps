1d06eba8d7075a531cd0756ac830e09425fb8c86
Wyatt <mrwnt10@gmail.com>
2018-12-24 16:40:30 -0600

    Add initial board.

--- challenge_4/client/src/components/Square.jsx
+++ challenge_4/client/src/components/Square.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const Square = ({ x, y }) => <span className="square">X</span>;
+const Square = ({ x, y }) => (
+  <span className="square">
+    <div className="circle" />
+  </span>
+);
 
 export default Square;

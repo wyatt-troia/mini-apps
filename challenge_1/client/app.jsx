7c4d14906b2b3e763d5f9004d5b95f18ea0ac566
Wyatt <mrwnt10@gmail.com>
2018-12-26 20:58:07 -0600

    Add title and top margin.

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { Container, Row, Col } from "react-bootstrap";
-import SearchForm from "./searchForm.jsx";
+import SearchForm from "./SearchForm.jsx";
 
 let element = (
   <Container>
-    <Row>
+    <Row className="mt-4">
       <Col md={12}>
+        <h1>Historical Events Finder</h1>
         <SearchForm />
       </Col>
       {/* <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col> */}

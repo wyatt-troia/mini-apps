a036ccd947e8c5f0e19a95d2a82213c3190e966e
Wyatt <mrwnt10@gmail.com>
2018-12-26 20:42:46 -0600

    Add React-Bootstrap.

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("root"));
+let element = (
+  <Container>
+    <Row>
+      <Col md={12}>md=4</Col>
+      {/* <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col> */}
+    </Row>
+  </Container>
+);
+
+ReactDOM.render(element, document.getElementById("root"));

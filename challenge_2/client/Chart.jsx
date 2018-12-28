36419f68a135b7f3efa89c85225c21be3a46bd94
Wyatt <mrwnt10@gmail.com>
2018-12-28 14:11:23 -0600

    Add Webpack, Babel, Express, Coindesk API call, and example chart.js chart.

---
+++ challenge_2/client/Chart.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Chart from "chart.js";
+
+class myChart extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    let ctx = document.getElementById("chart");
+    var myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
+      }
+    });
+  }
+  render() {
+    return <canvas id="chart" />;
+  }
+}
+
+export default myChart;
--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,39 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import MyChart from "./Chart.jsx";
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    console.log("App component mounted");
+    axios
+      .get("https://api.coindesk.com/v1/bpi/historical/close.json", {
+        params: {
+          start: "2018-01-01",
+          end: "2018-12-27"
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+      });
+  }
+  render() {
+    return (
+      <Container className="mt-5">
+        <Row>
+          <Col>
+            <h1>Bitcoin Value</h1>
+            <MyChart />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById("root"));

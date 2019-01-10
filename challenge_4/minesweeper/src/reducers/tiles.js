cf7b0e82f579341795ddf5cfe6b96514bf564e2f
Wyatt <mrwnt10@gmail.com>
2019-01-10 16:51:16 -0500

    Fix bug where clicking reset did not create a new random board.

--- challenge_4/minesweeper/src/reducers/tiles.js
+++ challenge_4/minesweeper/src/reducers/tiles.js
@@ -2,76 +2,80 @@ let rowLength = 10;
 let rowCount = 10;
 let mineCount = 10;
 
-// set up empty board
-let initialTiles = [];
-for (let i = 0; i < rowCount; i++) {
-  initialTiles.push([]);
-  for (let j = 0; j < rowLength; j++) {
-    initialTiles[i].push({ containsMine: false, clicked: false });
-  }
-}
-
-// plant mines at random locations
-let mineLocations = [];
-while (mineLocations.length < mineCount) {
-  let potentialLocation = Math.floor(Math.random() * (rowCount * rowLength));
-  if (!mineLocations.includes(potentialLocation)) {
-    mineLocations.push(potentialLocation);
-  }
-}
-mineLocations.forEach(location => {
-  let row = Math.floor(location / rowLength);
-  let col = location - row * rowLength;
-  initialTiles[row][col].containsMine = true;
-});
-
-// calculate adjacent mine counts for each tile
-const calculateAdjacentMineCount = (row, col) => {
-  let count = 0;
-  // count adjacent tiles in previous row
-  for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
-    if (
-      initialTiles[row - 1] &&
-      initialTiles[row - 1][searchCol] &&
-      initialTiles[row - 1][searchCol].containsMine
-    ) {
-      count++;
-    }
-  }
-  // count adjacent tiles in current row
-  for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
-    if (
-      initialTiles[row] &&
-      initialTiles[row][searchCol] &&
-      initialTiles[row][searchCol].containsMine
-    ) {
-      count++;
-    }
-  }
-  // count adjacent tiles in next row
-  for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
-    if (
-      initialTiles[row + 1] &&
-      initialTiles[row + 1][searchCol] &&
-      initialTiles[row + 1][searchCol].containsMine
-    ) {
-      count++;
+const generateInitialState = () => {
+  // set up empty board
+  let initialTiles = [];
+  for (let i = 0; i < rowCount; i++) {
+    initialTiles.push([]);
+    for (let j = 0; j < rowLength; j++) {
+      initialTiles[i].push({ containsMine: false, clicked: false });
     }
   }
 
-  return count;
+  // plant mines at random locations
+  let mineLocations = [];
+  while (mineLocations.length < mineCount) {
+    let potentialLocation = Math.floor(Math.random() * (rowCount * rowLength));
+    if (!mineLocations.includes(potentialLocation)) {
+      mineLocations.push(potentialLocation);
+    }
+  }
+  mineLocations.forEach(location => {
+    let row = Math.floor(location / rowLength);
+    let col = location - row * rowLength;
+    initialTiles[row][col].containsMine = true;
+  });
+
+  // calculate adjacent mine counts for each tile
+  const calculateAdjacentMineCount = (row, col) => {
+    let count = 0;
+    // count adjacent tiles in previous row
+    for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
+      if (
+        initialTiles[row - 1] &&
+        initialTiles[row - 1][searchCol] &&
+        initialTiles[row - 1][searchCol].containsMine
+      ) {
+        count++;
+      }
+    }
+    // count adjacent tiles in current row
+    for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
+      if (
+        initialTiles[row] &&
+        initialTiles[row][searchCol] &&
+        initialTiles[row][searchCol].containsMine
+      ) {
+        count++;
+      }
+    }
+    // count adjacent tiles in next row
+    for (let searchCol = col - 1; searchCol < col + 2; searchCol++) {
+      if (
+        initialTiles[row + 1] &&
+        initialTiles[row + 1][searchCol] &&
+        initialTiles[row + 1][searchCol].containsMine
+      ) {
+        count++;
+      }
+    }
+
+    return count;
+  };
+
+  for (let row = 0; row < rowCount; row++) {
+    for (let col = 0; col < rowLength; col++) {
+      initialTiles[row][col].adjacentMineCount = calculateAdjacentMineCount(
+        row,
+        col
+      );
+    }
+  }
+
+  return { result: "", tilesClicked: 0, tiles: initialTiles };
 };
 
-for (let row = 0; row < rowCount; row++) {
-  for (let col = 0; col < rowLength; col++) {
-    initialTiles[row][col].adjacentMineCount = calculateAdjacentMineCount(
-      row,
-      col
-    );
-  }
-}
-
-let initialState = { result: "", tilesClicked: 0, tiles: initialTiles };
+let initialState = generateInitialState();
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -146,7 +150,7 @@ export const reducer = (state = initialState, action) => {
       };
 
     case "RESET_GAME":
-      return initialState;
+      return generateInitialState();
 
     default:
       return state;

673d8531892f72d4fdd6bd166326ca7879ea62a1
Wyatt <mrwnt10@gmail.com>
2019-01-11 10:40:39 -0500

    Re-implement server.js.

---
+++ challenge_1/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const axios = require("axios");
+
+app.use(express.static("public"));
+
+app.get("/events", (req, res) => {
+  let { q, _page } = req.query;
+  let params;
+  if (_page) {
+    params = {
+      q,
+      _page
+    };
+  } else {
+    params = { q };
+  }
+  axios
+    .get("http://localhost:3000/events", {
+      params
+    })
+    .then(response => res.send(response.data));
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

d2954f247ed75adf9e9ce91f578575083ca9e9c1
Wyatt <mrwnt10@gmail.com>
2019-01-12 15:27:38 -0500

    Add build files due to slow AWS server being incapable of building.

---
+++ minesweeper/build/precache-manifest.50b93028326fe8e695bb57751d81728b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e14e1327fc7db0eed37e",
+    "url": "/static/js/main.e14e1327.chunk.js"
+  },
+  {
+    "revision": "7cbf09437ae5c76d7977",
+    "url": "/static/js/1.7cbf0943.chunk.js"
+  },
+  {
+    "revision": "e14e1327fc7db0eed37e",
+    "url": "/static/css/main.b88b2f22.chunk.css"
+  },
+  {
+    "revision": "af4a51675cd64cca11434bffc2c344e3",
+    "url": "/index.html"
+  }
+];
